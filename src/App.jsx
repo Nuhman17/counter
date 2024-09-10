9049251395bbce09010dd4f536948e4d2e05b754
Nuhman17 <muhammednuhmanck17@gmail.com>
2024-09-10 11:15:14 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Counter from './Counter'
+
+
+function App() {
+
+  return (
+    <>
+    <Counter/>
+
+    </>
+  )
+}
+
+export default App
